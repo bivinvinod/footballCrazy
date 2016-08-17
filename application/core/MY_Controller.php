2643ef053d685111ac59d77172de72fa0c85fd97
Bivin Vinod <bivinvinod@gmail.com>
2016-08-17 12:41:47 +0530

    bug fix

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -6,7 +6,6 @@ class MY_Controller extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->library('session');
         $this->load->helper('url');
     }
 
