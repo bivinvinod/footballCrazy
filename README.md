0b00dba731b07206cfaf0c0bad0518f120dbebdd
Bivin Vinod <bivinvinod@gmail.com>
2016-08-17 16:21:31 +0530

    bug fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# footballCrazy
-# footballCrazy
--- application/views/dashboard.php
+++
@@ -1,7 +0,0 @@
-<div class="right_col" role="main">
-<div class="page-title">
-		<div class="title_left">
-			<h3>Welcome</h3>
-		</div>
-	</div>
-</div>
