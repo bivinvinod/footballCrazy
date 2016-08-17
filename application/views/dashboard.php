b622b3eba214bb2187a68bb1fbf9d1198bf7f4c9
Bivin Vinod <bivinvinod@gmail.com>
2016-08-17 18:58:17 +0530

    bug fix

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -10,8 +10,6 @@
                   <div class="x_title">
                     <h2>Info <small></small></h2>
                     <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
                     </ul>
                     <div class="clearfix"></div>
                   </div>
@@ -21,7 +19,7 @@
                       <!-- blockquote -->
                       <blockquote>
                         <p>Hi guys. The Statistics in this site are taken from offical premier league website. Datas in this site are related to our league in FPL. Data is parsed from the offical site everytime you make a request. So there may be errors when their server is busy, also the site may be slow since we are taking data from their webiste every time. If you find any bugs or want to request any features etc.. please comment under my post in our group. The site is written in php(CI 3.0). If you like to contribute let me know. I hope you guys like it. Cheers!!</p>
-                        <footer>Bivin 
+                        <footer><cite title="Source Title">Bivin</cite>
                         </footer>
                       </blockquote>
 
