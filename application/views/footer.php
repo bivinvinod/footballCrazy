c4e96ed50ae75a8327f4778ee9eda1f4d6f919fd
Bivin Vinod <bivinvinod@gmail.com>
2016-08-17 13:15:33 +0530

    bug fix

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,13 +6,6 @@
 <!-- ========== -->
 <!-- Custom Theme Scripts -->
 <script src="<?php echo base_url(); ?>js/gentelella.min.js"></script>
-<script type="text/javascript">
-  $( document ).ready(function() {
-    <?php if($this->session->flashdata('msg')) {
-      echo "toastr.info('".$this->session->flashdata('msg')."')";
-    } ?>
-  });
-</script>
 
 
 </div>
