4e253cfe0d9d86b3e88dad369e374cf475127a9e
Bivin Vinod <bivinvinod@gmail.com>
2016-08-17 18:53:34 +0530

    dashboard comment

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,4 +4,47 @@
 			<h3>Welcome</h3>
 		</div>
 	</div>
+
+	<div class="col-md-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Typography <small>different design elements</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+
+                    <div class="col-md-8 col-lg-8 col-sm-7">
+                      <!-- blockquote -->
+                      <blockquote>
+                        <p>Hi guys. The Statistics in this site are taken from offical premier league website. Datas in this site are related to our league in FPL. Data is parsed from the offical site everytime you make a request. So there may be errors when their server is busy, also the site may be slow since we are taking data from their webiste every time. If you find any bugs or want to request any features etc.. please comment under my post in our group. The site is written in php(CI 3.0). If you like to contribute let me know. I hope you guys like it. Cheers!!</p>
+                        <footer>Bivin 
+                        </footer>
+                      </blockquote>
+
+                      
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    
+                  </div>
+                </div>
+              </div>
+
+
 </div>
